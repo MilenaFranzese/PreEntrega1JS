8090ffd399130976ecd6ba4d2ddbaaeb03c20902
milena franzese <milenafranzese95@gmail.com>
2023-11-09 11:34:58 -0300

    agrego funcion (correccion)

--- js/app.js
+++ js/app.js
@@ -1,31 +1,34 @@
 console.log("Conectado");
 
-let nombreUsuario = prompt("Bienvenido a Guarida de Lobos, por favor ingresá tu nombre:");
+function verificarEdad() {
+    let nombreUsuario = prompt("Bienvenido a Guarida de Lobos, por favor ingresá tu nombre:");
 
-while (!nombreUsuario || typeof nombreUsuario !== 'string' || !isNaN(nombreUsuario)) {
-    nombreUsuario = prompt("Por favor, ingresá un nombre válido:");
-}
-alert("Bienvenido " + nombreUsuario);
+    while (!nombreUsuario || typeof nombreUsuario !== 'string' || !isNaN(nombreUsuario)) {
+        nombreUsuario = prompt("Por favor, ingresá un nombre válido:");
+    }
+    alert("Bienvenido " + nombreUsuario);
 
-let edad;
+    let edad;
 
-while (true) {
-    let input = prompt("Ingrese su edad:");
+    while (true) {
+        let input = prompt("Ingrese su edad:");
 
-    // Me sirve para verificar si el input es un número y no está vacío
-    if (!isNaN(input) && input !== "") {
-        // Para convertir el input a número
-        edad = parseInt(input);
+        // Me sirve para verificar si el input es un número y no está vacío
+        if (!isNaN(input) && input !== "") {
+            // Para convertir el input a número
+            edad = parseInt(input);
 
-        if (edad >= 18) {
-            alert("Bienvenido, ¿qué curso o experiencia querés comprar hoy?");
-            break;
+            if (edad >= 18) {
+                alert("Bienvenido, ¿qué curso o experiencia querés comprar hoy?");
+                break;
+            } else {
+                alert("Sos menor de edad, recordá que para comprar necesitás una autorización de un adulto.");
+                break;
+            }
         } else {
-            alert("Sos menor de edad, recordá que para comprar necesitás una autorización de un adulto.");
-            break;
+            alert("Por favor, ingrese un valor numérico para la edad.");
         }
-    } else {
-        alert("Por favor, ingrese un valor numérico para la edad.");
     }
 }
 
+verificarEdad();
